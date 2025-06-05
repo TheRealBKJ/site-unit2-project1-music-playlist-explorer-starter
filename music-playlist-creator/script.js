898b4ff9962f8deb64e2656726ffcccc40116f70
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-04 23:12:13 -0700

    added json and songs about to start modals for playlist details

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -19,8 +19,52 @@ cards.forEach((card) => {
         openModal(title,image,artist);
     });
 });
+// modal stuff above this
 
 
+//logic for making cards
+
+function createCards(){
+    return fetch('data/data.json')
+    .then(response =>{
+        if (!response){
+            throw new Error("No playlists Added");
+        }
+        return response.json()
+    })
+    .then(data =>{
+        const cardGrid = document.getElementById('playlist-grid')
+        data.forEach(card =>{
+            const playlistcard = createCard(card)
+            cardGrid.appendChild(playlistcard)
+        })
+    })
+    .catch(error => console.error('Error Loading:', error));
+
+
+}
+
+function createCard(card){
+    const cardElement = document.createElement('article')
+    cardElement.classList.add('idcards')
+    cardElement.innerHTML = `
+        <img class="cardimg" src="${card.playlist_art}" alt="photo of playlist">
+        <h2 class="playlistTitle">${card.playlist_name}</h2>
+        <p class="authorTitle">${card.playlist_author}</p>
+        <button class="likeButton">
+            <img src="assets/img/heart.png" alt="like button">
+            <span>Like Count: 0</span>
+        </button>
+    `;
+    return cardElement;
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    createCards();
+})
+
+                
+// how to create a card and load them in!
 
 span.onclick = function() {
     modal.style.display = "none";
