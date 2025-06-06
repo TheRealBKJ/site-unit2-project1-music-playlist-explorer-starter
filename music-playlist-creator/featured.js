8fc1f9e896d5496f1a6ef8b83ddc9685bb9805d1
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-06 00:47:32 -0700

    featured page finished

--- music-playlist-creator/featured.js
+++ music-playlist-creator/featured.js
@@ -20,12 +20,42 @@ function buildFeatured(){
         const songs = document.getElementById('song-holder');
         const array = Array.from(data);
         const randomPlaylist = randomDisplay(array)
-        playlistInfo.innerHTML = randomPlaylist.playlist;
-        songs.innerHTML = randomPlaylist.songs
+        playlistInfo.innerHTML = `
+            <img id ="featured-image" src = "${randomPlaylist.playlist.img}" alt ="photo of playlist"> </img>
+            <h2 id="playlist-title"> ${randomPlaylist.playlist.title}<h2>
+        `;
+
+        songs.appendChild(randomPlaylist.songs);
     })
 }
 
 
 function randomDisplay(array){
+    const randInd = Math.floor(Math.random() * array.length);
+    const randData = array[randInd];
+    let songList =  document.createDocumentFragment();
+    randData.songs.forEach((song) =>{
+        const songBox = document.createElement('div');
+        songBox.className = 'song-box';
+        songBox.innerHTML = `
+            <img src="${song.image}" alt="Image of Song" class="song-thumbnail">
+            <div class="song-details">
+                <h4 id = 'song-title'> Song: ${song.song_name}</h4>
+                <h4 id = 'artist-name'> Artist: ${song.artist_name}<h4>
+                <h4 id = 'album-name'> Album: ${song.album_name}</h4>
+                <h4 id="duration"> Duration: ${song.duration}</h4>
+            </div>
+        `;
+        songList.appendChild(songBox);
+        })
+
+    return {
+        playlist: {
+            img : randData.playlist_art,
+            title : randData.playlist_name
+        },
+        songs: songList
+    }
+
 
 }
